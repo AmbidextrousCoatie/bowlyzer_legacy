19e5dd6a4490d337d1a937f7a02a216e80e9dcea
AmbidextrousCoatie <cfeller@gmx.de>
2026-05-18 14:11:54 +0200

    docker fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MobileNavProvider } from "./context/MobileNavContext";
 import { Sidebar } from "./components/Sidebar";
 import { ClubMatrix } from "./pages/diagnosis/ClubMatrix";
